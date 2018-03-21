17849ea05642faaabdd1417bf3b944463c34d646
cippero <gilwein@gmail.com>
2018-03-21 09:14:45 -0700

    Commit 4 - Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -31,3 +31,42 @@ for(var i=0; i<20; i++){
 		}
 	}
 }
+
+//Fizz Buzz
+//1.
+for(var i=1; i<=100; i++){
+	console.log(i);
+}
+
+//2.
+for(var i=1; i<=100; i++){
+	if (i % 3 === 0){
+		console.log("Fizz");
+	} else {
+		console.log(i);
+	}
+}
+
+//3.
+for(var i=1; i<=100; i++){
+	if (i % 3 === 0){
+		console.log("Fizz");
+	} else if (i % 5 === 0){
+		console.log("Buzz");
+	} else {
+		console.log(i);
+	}
+}
+
+//4.
+for(var i=1; i<=100; i++){
+	if (i % 3 === 0 && i % 5 === 0){
+		console.log("FizzBuzz");
+	} else if (i % 3 === 0){
+		console.log("Fizz");
+	} else if (i % 5 === 0){
+		console.log("Buzz");
+	} else {
+		console.log(i);
+	}
+}
